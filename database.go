f1112e74c96ce651dc23333f16d4d02b3acf69d7
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-11 23:19:26 +0700

    Add database.go

---
+++ database.go
@@ -0,0 +1,21 @@
+package gomysql
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:kmzway87aa@tcp(localhost:3306)/belajar_go")
+	if err != nil {
+		fmt.Println("error")
+		panic(err)
+	}
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
